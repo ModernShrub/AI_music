a9ed3ed0b193156ef3c3d4a3a02df0bcb319f290
ModernShrub <75245177+ModernShrub@users.noreply.github.com>
2020-12-03 21:14:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,8 +15,6 @@ function setup() {
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
 }
-scoreRightWrist = 0;
-scoreLeftWrist = 0;
 
 rightWristX = 0;
 rightWristY = 0;
@@ -29,11 +27,33 @@ function setup() {
 	canvas.center();
 
 	video = createCapture(VIDEO);
-	video.hide();
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
+
 function draw() {
 	image(video, 0, 0, 600, 500);
 
 	fill("#FF0000");
     stroke("#FF0000");
 }
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+  
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    { 
+      rightWristX = results[0].pose.rightWrist.x;
+      rightWristY = results[0].pose.rightWrist.y;
+      console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+  
+      leftWristX = results[0].pose.leftWrist.x;
+      leftWristY = results[0].pose.leftWrist.y;
+      console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+          
+    }
+  }
